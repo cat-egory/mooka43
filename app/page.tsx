3daf01953941a96c79e0395ea9a9df3ac12530d2
cat-egory <xtheblackeyes@gmail.com>
2023-08-21 16:20:28 +0900

    #STYLE: make input area

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,12 @@ import { demos } from '../lib/demos'
 
 export default function Page() {
   return (
-    <div className="space-y-8">
-      <div className="font-bold">
-        <input placeholder="책 제목 또는 영화 제목을 입력해주세요." />
+    <div className='mx-20 space-y-8'>
+      <div className='box-border border-spacing-3 border-2 border-solid border-orange-400 p-5'>
+        <input
+          className='w-80'
+          placeholder='책 제목 또는 영화 제목을 입력해주세요.'
+        />
         <button>입력</button>
       </div>
       <div>귀여운 필터버튼</div>
